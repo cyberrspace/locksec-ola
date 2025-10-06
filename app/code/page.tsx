f28e26df1f8d3d8b9246fa55b04a95a4c9b401c0
cyberrspace <xhamooel95@gmail.com>
2025-10-06 10:48:34 +0100

    suspense used in code details

--- app/code/page.tsx
+++ app/code/page.tsx
@@ -3,25 +3,21 @@
 import ContentWrapper from "@/components/common/ContentWrapper";
 import CodeDetails from "@/components/GeneratedPage/CodeDetails";
 import CodeHero from "@/components/GeneratedPage/CodeHero";
+import { Suspense } from "react";
 
-
-
-export default function CodePage(){
-  return(
+export default function CodePage() {
+  return (
     <ContentWrapper>
       <main className="h-[863px]">
         <section>
           <CodeHero />
         </section>
-        <section>
-          <CodeDetails />
-        </section>
- 
-      </main>
-     
-    
-    </ContentWrapper>
- 
 
-  )
+        {/* Wrap CodeDetails properly inside Suspense */}
+        <Suspense fallback={<div className="text-center p-6">Loading...</div>}>
+          <CodeDetails />
+        </Suspense>
+      </main>
+    </ContentWrapper>
+  );
 }
