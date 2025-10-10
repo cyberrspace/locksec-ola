415d314e71251f447d2e0d3d50faac771e6976b3
cyberrspace <xhamooel95@gmail.com>
2025-10-10 16:39:28 +0100

    page side updated

--- components/common/ContentWrapper.tsx
+++ components/common/ContentWrapper.tsx
@@ -9,7 +9,7 @@ interface ContentWrapperProps {
 export default function ContentWrapper({ children }: ContentWrapperProps) {
   return (
     <div
-      className="full min-h-screen rounded-[9px] opacity-100 bg-white shadow-md mx-auto"
+      className="w-full min-h-screen rounded-[9px] opacity-100 bg-white shadow-md mx-auto"
       style={{ transform: "rotate(0deg)" }}>
       {children}
     </div>
